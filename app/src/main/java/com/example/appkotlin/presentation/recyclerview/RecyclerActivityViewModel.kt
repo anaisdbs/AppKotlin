e51a24775e8cac181a75be4afe02cb9ea7fe2c0b
anaisdbs <62145692+anaisdbs@users.noreply.github.com>
2020-12-28 19:36:39 +0100

    Refractor Recycler with a ViewModel

---
+++ app/src/main/java/com/example/appkotlin/presentation/recyclerview/RecyclerActivityViewModel.kt
@@ -0,0 +1,44 @@
+package com.example.appkotlin.presentation.recyclerview
+
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.appkotlin.data.remote.RecyclerList
+import com.example.appkotlin.data.remote.api.RetroInstance
+import com.example.appkotlin.data.remote.api.RetroService
+import retrofit2.Call
+import retrofit2.Response
+
+class RecyclerActivityViewModel: ViewModel() {
+
+    lateinit var recyclerListData: MutableLiveData<RecyclerList>
+
+    init {
+        recyclerListData = MutableLiveData()
+    }
+
+    fun getRecyclerListDataObserver(): MutableLiveData<RecyclerList>{
+        return recyclerListData
+    }
+
+    fun makeApiCall(){
+        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
+        val call = retroInstance.getDataFromApi("tetris")
+        call.enqueue(object : retrofit2.Callback<RecyclerList>{
+            override fun onResponse(call: Call<RecyclerList>, response: Response<RecyclerList>) {
+                if(response.isSuccessful) {
+
+                    recyclerListData.postValue(response.body())
+                }else{
+                    recyclerListData.postValue(null)
+                }
+            }
+            override fun onFailure(call: Call<RecyclerList>, t: Throwable) {
+                recyclerListData.postValue(null)
+            }
+
+        })
+    }
+
+}
+
--- app/src/main/java/com/example/appkotlin/presentation/recyclerview/RecyclerViewActivity.kt
+++ app/src/main/java/com/example/appkotlin/presentation/recyclerview/RecyclerViewActivity.kt
@@ -3,6 +3,9 @@ package com.example.appkotlin.presentation.recyclerview
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager.VERTICAL
@@ -43,19 +46,16 @@ class RecyclerViewActivity : AppCompatActivity() {
 
     fun createData(){
 
-        val retroInstance = RetroInstance.getRetroInstance().create(RetroService::class.java)
-        val call = retroInstance.getDataFromApi("newyork")
-        call.enqueue(object : retrofit2.Callback<RecyclerList>{
-            override fun onResponse(call: Call<RecyclerList>, response: Response<RecyclerList>) {
-                if(response.isSuccessful) {
-                    recyclerViewAdapter.setListData(response.body()?.items!!)
-                    recyclerViewAdapter.notifyDataSetChanged()
-                }
-            }
-            override fun onFailure(call: Call<RecyclerList>, t: Throwable) {
+       val viewModel = ViewModelProviders.of(this).get(RecyclerActivityViewModel::class.java)
+        viewModel.getRecyclerListDataObserver().observe(this, Observer<RecyclerList>{
+            if(it != null){
+                recyclerViewAdapter.setListData(it.items)
+                recyclerViewAdapter.notifyDataSetChanged()
+
+            }else{
                 Toast.makeText(this@RecyclerViewActivity, "Error in getting data from api.", Toast.LENGTH_LONG).show()
             }
         })
-
+        viewModel.makeApiCall()
     }
 }
